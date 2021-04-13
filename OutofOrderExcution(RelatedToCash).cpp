ed0cd32f08e204324c99aa61f140d29418a860cf
KwonSangMin <38064756+KwonSangMin@users.noreply.github.com>
2021-04-13 15:35:39 +0900

    Create OutofOrderExcution(RelatedToCash).cpp
    위의 코드의 결과값을 살펴보면 Arr1[10000]=2956 Arr2[2956] = 6110이라는 결과값이 나온다.
    해당 결과값이 의미하는 바는 각 쓰레드가 a와 b값을 참조할 때, 메모리에 있는 값을 참조하는 것이 아니라 각 cpu의 캐시에 등록되어있는 값을 참조한다는 것을 알 수 있다.
    다시 말해서, 만약 각 쓰레드가 a와 b값을 갱신할 때마다 매번 메모리에 저장하고 a와 b값을 읽어올 때도 메모리에서 읽어온다면 즉 변수 a와 b의 값을 참조 및 쓰기 작업 시 원자적으로 수행하지 않는다면, 쓰레드 A에서 참조하는 b값이 쓰레드 B의 b값과 달라질 수 있다 ( writebuffer에 작성하고 메모리에는 업로드하지 않기 때문)

---
+++ OutofOrderExcution(RelatedToCash).cpp
@@ -0,0 +1,36 @@
+using namespace std;
+int a = 0;
+int b = 0;
+
+#define MAX 1000000
+int arr1[MAX];
+int arr2[MAX];
+void func1()
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        a = i;
+        arr1[i] = b;
+    }
+}
+void func2()
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        b = i;
+        arr2[i] = a;
+    }
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+    thread A(func1), B(func2);
+    A.join();
+    B.join();
+    for (int i = 0; i < MAX; i+=10000)
+    {
+        int a1value = arr1[i];
+        int a2ValueAti = arr2[a1value];
+        cout << "Arr1["<<i << "] == " <<a1value<<" arr2["<<a1value<<"] == " << a2ValueAti<<endl;
+    }
+}
